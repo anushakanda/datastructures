2fbdd59243911818fbb930f8004c2c16102378b1
anushakanda <71995197+anushakanda@users.noreply.github.com>
2023-08-27 12:51:59 +0530

    Create ReverseLinkedList.java

---
+++ linkedlist/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+/**
+* Reverse a linked list. Do it in-place and in one-pass.
+*For example:
+*
+*Given 1->2->3->4->5->NULL,
+*
+* return 5->4->3->2->1->NULL.
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode A) {
+        ListNode prev = null;
+        ListNode curr = A;
+        ListNode next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;   
+    }
+}
