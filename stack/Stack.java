a2c5c5f25da933369782238658aae75fb9972947
anushakanda <71995197+anushakanda@users.noreply.github.com>
2023-09-05 13:29:31 +0530

    Create Stack.java

---
+++ stack/Stack.java
@@ -0,0 +1,38 @@
+public class Stack {
+  private static class Node{
+    int data;
+    Node next;
+    Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  private Node top;
+
+  public boolean isEmpty(){
+    return top == null
+  }
+
+  public int peek(){
+    if(top == null){
+      //stack is empty.
+    }
+    return top.data
+  }
+
+  public void push(int data){
+    Node newNode = new Node(data);
+    newNode.next = top;
+    top = newNode;
+  }
+
+  public int pop(){
+    if(top == null){
+      // Stack is empty
+    }
+    top = top.next;
+    return top.data;
+  }
+  
+}
