9f3d1eb693c9d40f0517898a978cfff6805f31e0
anushakanda <71995197+anushakanda@users.noreply.github.com>
2023-09-05 13:15:34 +0530

    Create Queue.java

---
+++ queue/Queue.java
@@ -0,0 +1,54 @@
+/**
+* Queue: FIFO
+* https://www.youtube.com/watch?v=wjI1WNcIntg
+/**
+public class Queue {
+
+public sttaic class Node {
+   int data;
+   Node next;
+  Node(int data){
+    this.data = data;
+    this.next = null;
+  }
+}
+private Node head; //Remove elements from here.
+private Node tail; //Add elements from here.
+
+  public boolean isEmpty(){
+    return head == null;
+  }
+
+  public int peek(){
+    if(head == null){
+      //Queue is Empty
+    }
+
+    return head.data;
+  }
+  
+  public void add(int data){
+    Node newNode = Node(data);
+    if(tail != null){
+      tail.next = newNode;
+    }
+    tail = newNode;
+    if(head == null){
+      tail = newNode;
+    }
+  }
+
+  public int remove(){
+    if(head == null){
+      // Queue is Empty.
+    }
+    int return = head.data;
+    head = head.next;
+     if(head == null){
+     tail == null;
+    }
+    
+  }
+  
+
+}
