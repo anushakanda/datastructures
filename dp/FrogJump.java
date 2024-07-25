2822c30851f3c9129078835fbba35e5a07e897a7
anushakanda <71995197+anushakanda@users.noreply.github.com>
2024-07-25 22:21:09 +0530

    Update FrogJump.java

--- dp/FrogJump.java
+++ dp/FrogJump.java
@@ -59,3 +59,51 @@ public static int frogJump(int n, int heights[]) {
         int energyLost2 = Math.abs(heights[n-1]-heights[n-3]);
         frogJump(n-2, heights, minTotalEnergy+energyLost2,allPossibleWays);
     }
+
+// Approach 2: Recursive
+public static int frogJump(int n, int heights[]) {
+        if(n==1) return 0;
+        if(n==2) return Math.abs(heights[1]-heights[0]);      
+        
+        int step1Enrg = frogJump(n-1, heights)+Math.abs(heights[n-1]-heights[n-2]);
+        int step2Enrg = frogJump(n-2, heights)+Math.abs(heights[n-1]-heights[n-3]);
+
+        return Math.min(step1Enrg, step2Enrg);
+    }
+
+//Approach 3: Memorization
+public static int frogJump(int n, int heights[]) {
+       int[] dp = new int[n];
+       Arrays.fill(dp,-1);
+       frogJump(n, heights, dp);
+       return dp[n-1];
+    }
+
+    public static int frogJump(int n, int heights[], int[] dp) {
+        if(n==1) return dp[0] = 0;
+        if(n==2) return dp[1] = Math.abs(heights[1]-heights[0]);  
+
+        if(dp[n-1]!=-1) return dp[n-1];
+        
+        int step1Enrg = frogJump(n-1, heights)+Math.abs(heights[n-1]-heights[n-2]);
+        int step2Enrg = frogJump(n-2, heights)+Math.abs(heights[n-1]-heights[n-3]);
+
+        return dp[n-1] = Math.min(step1Enrg, step2Enrg);
+    }
+
+// Approach 4:Tabulation
+public static int frogJump(int n, int heights[]) {
+       int[] dp = new int[n];
+       Arrays.fill(dp,-1);
+       dp[0] = 0;
+       dp[1] = Math.abs(heights[1]-heights[0]); 
+
+       for(int i=2;i<n;i++){
+           int first = dp[i-1]+Math.abs(heights[i]-heights[i-1]);
+           int second = dp[i-2]+Math.abs(heights[i]-heights[i-2]);
+           dp[i] = Math.min(first,second);
+       }
+       return dp[n-1];
+    }
+
+
